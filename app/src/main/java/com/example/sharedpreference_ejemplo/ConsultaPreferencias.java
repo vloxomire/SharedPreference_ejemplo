bc7d1de40c8aa23f02770fd6c534c0380bf8ac64
max <40814622+vloxomire@users.noreply.github.com>
2018-10-10 10:30:32 -0300

    1030

--- app/src/main/java/com/example/sharedpreference_ejemplo/ConsultaPreferencias.java
+++ app/src/main/java/com/example/sharedpreference_ejemplo/ConsultaPreferencias.java
@@ -1,14 +1,9 @@
 package com.example.sharedpreference_ejemplo;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import java.util.Map;
-import java.util.Set;
 
 public class ConsultaPreferencias extends AppCompatActivity {
 private TextView txtResulUser,txtResulPass;
